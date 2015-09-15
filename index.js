ffb5a0b988f9a1347490a8c770936a38fcf173cd
Amit Periyapatna <amperiya@cisco.com>
2015-09-15 14:31:22 +0530

    Added new features of Chat names

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var express = require("express");
+var cookieParser = require("cookie-parser");
+var cookies = require("cookies");
 var app = express();
 var port = 3700;
 
 app.set('views',__dirname + '/tpl');
 app.set('view engine',"jade");
 app.engine('jade', require('jade').__express);
+app.use(cookieParser());
+var names = ["dumbass","jackass","asshole"];
 app.get("/", function(req,res){
+	var userName = names[Math.floor(Math.random()*names.length)];
+	res.cookie("userName" , userName);
 	res.render("page");
+	console.log("Cookies: " , req.cookies);
+
 });
 app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function(socket){
-	socket.emit('message', { message : 'welcome to the chat' });
+	socket.emit('message', { message : 'welcome to the chat' , user : 'bot' });
 	socket.on('send',function (data) {
 		io.sockets.emit('message',data);
 	});
--- public/chat.js
+++ public/chat.js
@@ -3,21 +3,35 @@ var show_typing;
 
 window.onload = function() {
 var messages = [];
+var users = [];
 var socket = io.connect('http://173.36.55.149:3700');
 var field = document.getElementById("field");
 var sendButton = document.getElementById("send");
 var content = document.getElementById("content");
 var typing = document.getElementById("typing"); 
+var currentUser = readCookie('userName');
  
     socket.on('message', function (data) {
         if(data.message) {
             messages.push(data.message);
+            users.push(data.user);
             var html = '';
+            var textAlign = '';
             for(var i=0; i<messages.length; i++) {
-                html += '<div id="message">' + messages[i] + '</div>';
+              if(data.users[i]){
+                if(data.users[i] == currentUser)
+                  textAlign = 'style="text-align: left"';
+                else
+                  textAlign = 'style="text-aligh: right"';
+                html += '<div id="message">' + users[i] + messages[i] + '</div>';
+              }
+              else{
+                html += '<div id="message" style="text-align: right"> Ghost: ' + message[i] + '</div>';
+              }
             }
             content.innerHTML = html;
 	    content.scrollTop = content.scrollHeight;
+      console.log(readCookie('userName'));
 	    notifyMe(data.message);
 	    document.title = "Realtime Chat Application";
         } else {
@@ -33,7 +47,7 @@ var typing = document.getElementById("typing");
     };
     send_message = function() {
 	var text = field.value;
-	socket.emit('send',{message : text});
+	socket.emit('send',{message : text, user : readCookie('userName')});
 	field.value = '';
    } 
    show_typing = function() {
@@ -41,11 +55,25 @@ var typing = document.getElementById("typing");
    }
 }
 
-  function runScript(e){
+function runScript(e){
 	if(e.keyCode==13)
 	send_message();
 }
 function onChange(){
   console.log("Here");
   show_typing();
+}
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name,"",-1);
 }
