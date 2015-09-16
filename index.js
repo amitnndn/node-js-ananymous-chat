1693ae0f63e0de3a9730f1fabb13bc6c0a42a183
Amit Periyapatna <amperiya@cisco.com>
2015-09-16 13:33:28 +0530

    Resolving bugs

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ app.get("/", function(req,res){
 	var userName = names[Math.floor(Math.random()*names.length)];
 	//removeElement(userName);
 	var cookie = res.cookies.userName;
-	if(cookie == undefined){
+	if(!cookie){
 		res.cookie("userName" , userName);
-		res.render("page");
 		console.log("Cookies: " , req.cookies);
 	}
+	res.render("page");
 });
 app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function(socket){
-	socket.emit('message', { message : 'welcome to the chat' , user : 'bot' });
+	//socket.emit('message', { message : 'welcome to the chat' , user : 'bot' });
 	socket.on('send',function (data) {
 		io.sockets.emit('message',data);
 	});
