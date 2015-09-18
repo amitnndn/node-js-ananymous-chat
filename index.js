64d4aab6461c74e73426835c53b339f0348cd3e7
Amit Periyapatna <amperiya@cisco.com>
2015-09-18 12:22:06 +0530

    resolved bugs

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ var names = ["dumbass","jackass","asshole"];
 app.get("/", function(req,res){
 	var userName = names[Math.floor(Math.random()*names.length)];
 	//removeElement(userName);
-	var cookie = res.cookies.userName;
-	if(!cookie){
-		res.cookie("userName" , userName);
+	var cookie = req.cookies.userName;
+	if(cookie == undefined){
+		res.cookie("userName" , userName, {expires: new Date(new Date().getTime()+86409000)});
 		console.log("Cookies: " , req.cookies);
 	}
 	res.render("page");
